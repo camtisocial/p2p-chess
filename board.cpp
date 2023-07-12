2c8e482d98c70c422085b59ec2f2965dcc9243d3
Cameron <cameron.thompson3@g.austincc.edu>
2023-07-12 17:17:23 -0500

    improved memory error still needs work

--- board.cpp
+++ board.cpp
@@ -2,10 +2,15 @@
 #include "board.h"
 
 GameBoard::GameBoard() {
+    vector<Square*> tmpVec;
     int color = 1;
-    for (int i{}; i<7; i++) {
-        for (int j{}; j<7; j++) {
+    for (int i{}; i<8; i++) {
+        for (int j{}; j<8; j++) {
+
             Square *newSquare = new Square;
+            //ChessPiece *newPiece = new ChessPiece; 
+            //newSquare->current = *newPiece;
+
 
             // 0 = black : 1 = white
             if (color%2 == 0) {
@@ -13,27 +18,36 @@ GameBoard::GameBoard() {
             } else {
                 newSquare->color = 1;
             }
-            newSquare->current = pawn
 
-            //board[i][j] = newSquare;
-            //color++;
+            tmpVec.push_back(newSquare);
+            color++;
+
+            if (j == 7) {
+                board.push_back(tmpVec);
+            }
+
         }
     }
+    std::cout << "This ---> " << color << std::endl;
 }
 
 GameBoard::~GameBoard() {
-    for (int i{}; i<7; i++) {
-        for (int j{}; j<7; j++) {
+    for (int i{}; i<8; i++) {
+        for (int j{}; j<8; j++) {
+            std::cout << "I'm gay" << std::endl;
             delete board[i][j];
+            //delete board[j];
         }
     }
 }
 
+        
+
 
 void GameBoard::printBoard() {
-    for (int i{}; i<7; i++) {
+    for (int i{}; i<8; i++) {
         std::cout << std::endl;
-        for (int j{}; j<7; j++) {
+        for (int j{}; j<8; j++) {
             std::cout << board[i][j]->current.name << " ";
         }
     }
