32ba42c34c7c78f5e785acc6dd0f2bb11fe448bb
Cameron <cameron.thompson3@g.austincc.edu>
2023-07-12 17:16:47 -0500

    added basic testing for class creation and displaying board

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 #include "main.h"
 
 int main(int argc, char** argv) {
+
+    std::cout << "creating new board..." << std::endl;
+    GameBoard board;
+    std::cout << "done"<< std::endl << std::endl;
+
+    std::cout << "displaying empty board " << std::endl;
+    board.printBoard();
+
+
 	return 0;
 }
 
