fbae3709e127e8d48d0d8681a1b5a7f582ca272b
Cameron <cameron.thompson3@g.austincc.edu>
2023-07-19 16:57:36 -0500

    created constructors/destructors for all piece types. Added color char variable

--- chessPiece.cpp
+++ chessPiece.cpp
@@ -1,16 +1,48 @@
-
 #include "chessPiece.h"
 
-
 ChessPiece::ChessPiece() {
+    color = 'E';
 }
-
 ChessPiece::~ChessPiece() {
-
 }
+
 
 Pawn::Pawn() {
+    color = 'E';
+}
+Pawn::~Pawn() {
 }
 
-Pawn::~Pawn() {
+
+Knight::Knight() {
+    color = 'E';
+}
+Knight::~Knight() {
+}
+
+
+Bishop::Bishop() {
+    color = 'E';
+}
+Bishop::~Bishop() {
+}
+
+Rook::Rook() {
+    color = 'E';
+}
+Rook::~Rook() {
+}
+
+
+Queen::Queen() {
+    color = 'E';
+}
+Queen::~Queen() {
+}
+
+
+King::King() {
+    color = 'E';
+}
+King::~King() {
 }
--- chessPiece.h
+++ chessPiece.h
@@ -4,25 +4,87 @@
 
 class ChessPiece {
     public:
-        ChessPiece();
-        ~ChessPiece();
         char color;
         virtual char getName() {
             return 'X';
         }
+
+        ChessPiece(); 
+        ~ChessPiece();
     private:
 };
 
 class Pawn: public ChessPiece {
     public:
-        Pawn();
-        ~Pawn();
         char color;
         virtual char getName() {
             return 'P';
         }
         
+        Pawn();
+        ~Pawn();
     private:
 };
 
+class Knight: public ChessPiece {
+    public:
+        char color;
+        virtual char getName() {
+            return 'N';
+        }
+        
+        Knight();
+        ~Knight();
+    private:
+};
+
+class Bishop: public ChessPiece {
+    public:
+        char color;
+        virtual char getName() {
+            return 'B';
+        }
+
+        Bishop();
+        ~Bishop();
+        
+    private:
+};
+
+class Rook: public ChessPiece {
+    public:
+        char color;
+        virtual char getName() {
+            return 'R';
+        }
+        
+        Rook();
+        ~Rook();
+
+    private:
+};
+
+class Queen: public ChessPiece {
+    public:
+        char color;
+        virtual char getName() {
+            return 'Q';
+        }
+        
+        Queen();
+        ~Queen();
+    private:
+};
+
+class King: public ChessPiece {
+    public:
+        char color;
+        virtual char getName() {
+            return 'K';
+        }
+        
+        King();
+        ~King();
+    private:
+};
 #endif
