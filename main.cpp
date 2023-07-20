8c41151e9d6a9cd13ee63c6cff49019398caada1
Cameron <cameron.thompson3@g.austincc.edu>
2023-07-20 11:17:33 -0500

    added basic event loop

--- main.cpp
+++ main.cpp
@@ -3,12 +3,22 @@
 
 int main(int argc, char** argv) {
 
-    std::cout << "creating new board..." << std::endl;
+   // std::cout << "creating new board...";
     GameBoard board;
-    std::cout << "done"<< std::endl << std::endl;
+    //std::cout << "done"<< std::endl << std::endl;
 
-    std::cout << "displaying empty board " << std::endl;
-    board.printBoard();
+  //  std::cout << "displaying board...\n " << std::endl;
+//    board.printBoard();
+    std::cout << std::endl;
+
+    std::string q = "";
+    while(q != "q") {
+        board.printBoard();
+        std::cout << "\nEnter move: ";
+        std::cin >> q;
+        std::cout << "\n";
+
+    }
 
 
 	return 0;
