9f20fa1a20b561b9090402cdc5db7f07cf387bb0
cameron <thompsonca99@gmail.com>
2023-08-02 00:09:21 -0500

    fixed warning from printing title, updated getting user input to use getline and added piece movement to game loop

--- main.cpp
+++ main.cpp
@@ -16,18 +16,21 @@ int main(int argc, char** argv) {
         system("clear");
 
 
-    std::cout << R"(
+        std::cout << R"(
          _                   
      ___| |__   ___  ___ ___  
-    / __| '_ \ / _ \/ __/ __| 
-   | (__| | | |  __/\__ \__ \ 
+    / __| '_ \ / _ \/ __/ __|
+   | (__| | | |  __/\__ \__ \
     \___|_| |_|\___||___/___/ )";
+
 
         std::cout << "\n\n\n";
         board.printBoard();
         std::cout << "\n\n";
         std::cout << "   Enter move: ";
-        std::cin >> q;
+        std::getline(std::cin,  q);
+        //std::cin >> q;
+        board.movePiece(q);
         std::cout << "\n";
 
     }
