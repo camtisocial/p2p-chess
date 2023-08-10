40fbc27cece4fd1adec800f52ac2669c91a4c39a
Cameron <cameron.thompson3@g.austincc.edu>
2023-08-09 20:26:09 -0500

    moved dependencies to topmost file

--- moveData.h
+++ moveData.h
@@ -1,6 +1,13 @@
 #ifndef MOVE_DATA_H
 #define MOVE_DATA_H
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <memory>
+#include <sstream>
+
 struct MoveData {
     int row;
     int column;
