f0aef121913a38ca0ed7201f255c473716b0088d
Cameron <cameron.thompson3@g.austincc.edu>
2024-05-15 11:18:58 -0500

    added bounds checking within the getLegalMoves function for pawns

--- chessPiece.cpp
+++ chessPiece.cpp
@@ -24,34 +24,42 @@ vector<std::shared_ptr<MoveData>> Pawn::getLegalMoves(vector<vector<std::shared_
     }
 
     //checks if pawn has moved yet and if squares ahead are empty
-    if(board[row-1*colorSwitch][column]->getName() == 'X') {
-        std::shared_ptr<MoveData> newMove(new MoveData);
-        newMove->row = row-1*colorSwitch;
-        newMove->column = column;
-        legalMoves.push_back(newMove);
+    if(row-1*colorSwitch >= 0 && row-1*colorSwitch < board.size()) {
+        if(board[row-1*colorSwitch][column]->getName() == 'X') {
+            std::shared_ptr<MoveData> newMove(new MoveData);
+            newMove->row = row-1*colorSwitch;
+            newMove->column = column;
+            legalMoves.push_back(newMove);
+        }
     }
-    if(board[row-2*colorSwitch][column]->getName() == 'X' && board[row-1*colorSwitch][column]->getName() == 'X' && moved == false) {
-        std::shared_ptr<MoveData> newMove(new MoveData);
-        newMove->row = row-2*colorSwitch;
-        newMove->column = column;
-        legalMoves.push_back(newMove);
+    if(row-2*colorSwitch >= 0 && row-2*colorSwitch < board.size()) {
+        if(board[row-2*colorSwitch][column]->getName() == 'X' && board[row-1*colorSwitch][column]->getName() == 'X' && moved == false) {
+            std::shared_ptr<MoveData> newMove(new MoveData);
+            newMove->row = row-2*colorSwitch;
+            newMove->column = column;
+            legalMoves.push_back(newMove);
+        }
     }
     //first checks if horizontal moves are in bounds
     //then checks if diagonal squares are occupied by opposite color pieces 
     if (column > 0) {
-        if(board[row-1*colorSwitch][column-1]->getName() != 'X' && board[row-1*colorSwitch][column-1]->getColor() != color) {
-            std::shared_ptr<MoveData> newMove(new MoveData);
-            newMove->row = row-1*colorSwitch;
-            newMove->column = column-1;
-            legalMoves.push_back(newMove);
+        if(row-1*colorSwitch >= 0 && row-1*colorSwitch < board.size()) {
+            if(board[row-1*colorSwitch][column-1]->getName() != 'X' && board[row-1*colorSwitch][column-1]->getColor() != color) {
+                std::shared_ptr<MoveData> newMove(new MoveData);
+                newMove->row = row-1*colorSwitch;
+                newMove->column = column-1;
+                legalMoves.push_back(newMove);
+            }
         }
     }
-    if (column < 8) {
-        if(board[row-1*colorSwitch][column+1]->getName() != 'X' && board[row-1*colorSwitch][column+1]->getColor() != color) {
-            std::shared_ptr<MoveData> newMove(new MoveData);
-            newMove->row = row-1*colorSwitch;
-            newMove->column = column+1;
-            legalMoves.push_back(newMove);
+    if (column < board[0].size()-1) {
+        if(row-1*colorSwitch >= 0 && row-1*colorSwitch < board.size()) {
+            if(board[row-1*colorSwitch][column+1]->getName() != 'X' && board[row-1*colorSwitch][column+1]->getColor() != color) {
+                std::shared_ptr<MoveData> newMove(new MoveData);
+                newMove->row = row-1*colorSwitch;
+                newMove->column = column+1;
+                legalMoves.push_back(newMove);
+            }
         }
     }
     //for (auto b: legalMoves) {
