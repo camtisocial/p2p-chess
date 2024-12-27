05fd057c1d900ed96048957af963de43d039f702
cameron <thompsonca99@gmail.com>
2024-12-27 16:39:24 -0600

    first version of serializing function using nlohmann

---
+++ network.h
@@ -0,0 +1,10 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include "chessPiece.h"
+#include <nlohmann/json.hpp>
+
+void serializeBoard(vector<vector<std::shared_ptr<ChessPiece>>> board, int turnNum, int playerTurn, nlohmann::json &jsonObject);
+void deSerializeBoard(vector<vector<std::shared_ptr<ChessPiece>>> board, int turnNum, int playerTurn, nlohmann::json &jsonObject);
+
+#endif //NETWORK_H
